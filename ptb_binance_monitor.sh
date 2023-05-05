f387667c88e18cb2e7f3d6579d3a97351b462da5
Sergey <20flint12@gmail.com>
2023-05-05 19:32:54 +0200

    main++ read

--- ptb_binance_monitor.sh
+++ ptb_binance_monitor.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 cd /home/ubuntu/BinanceTrader
+read
 
 echo "--- attach session ---"
 #tmux new -s ptb_binance_monitor
 #tmux a -t ptb_binance_monitor
 tmux attach-session -t ptb_binance_monitor
+read
 
 # You are in the root ...
 cd /home/ubuntu/BinanceTrader
+read
 
 #echo "--- update repo ---"
 #git reset --hard origin/develop
@@ -18,9 +21,9 @@ cd /home/ubuntu/BinanceTrader
 
 echo "--- run env ---"
 source venv310/local/bin/activate
+read
 
 echo "--- run python script ---"
 python -m src.PTB_bot.ptb_binance_monitor
-
 
 
