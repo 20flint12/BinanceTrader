0d6d7164789e815ba55e4d95d644a8e9ce9712ac
Sergey <20flint12@gmail.com>
2023-05-05 19:40:35 +0200

    main++ read+

--- ptb_binance_monitor.sh
+++ ptb_binance_monitor.sh
@@ -4,20 +4,12 @@ cd /home/ubuntu/BinanceTrader
 read
 
 echo "--- attach session ---"
-#tmux new -s ptb_binance_monitor
-#tmux a -t ptb_binance_monitor
 tmux attach-session -t ptb_binance_monitor
 read
 
 # You are in the root ...
 cd /home/ubuntu/BinanceTrader
 read
-
-#echo "--- update repo ---"
-#git reset --hard origin/develop
-#git fetch
-#git switch develop
-#git pull origin develop
 
 echo "--- run env ---"
 source venv310/local/bin/activate
