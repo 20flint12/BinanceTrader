a5ccf658b1c3d79eb2d35fb6f4ec71a664c4c02b
Sergey <20flint12@gmail.com>
2023-05-05 16:26:56 +0200

    ptb_binance_monitor.sh++++

--- ptb_binance_monitor.sh
+++ ptb_binance_monitor.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#
+echo "--- open session ---"
+tmux new -s ptb_binance_monitor
+
+
+# You are in the root ...
+# cd BinanceTrader
+
 echo "--- update repo ---"
 git fetch
 git switch develop
