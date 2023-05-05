a36d6df3ba880d9dfd3a887f300c9e1636abebb2
Sergey <20flint12@gmail.com>
2023-05-05 15:39:36 +0200

    ptb_binance_monitor.sh+

--- ptb_binance_monitor.sh
+++ ptb_binance_monitor.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "Hello, World!"
-ls -l
+echo "--- update repo ---"
+git fetch
+git switch develop
+git pull origin develop
+
+echo "--- run env ---"
+source venv310/local/bin/activate
+
+echo "--- run python script ---"
+python -m src.PTB_bot.ptb_binance_monitor
+
