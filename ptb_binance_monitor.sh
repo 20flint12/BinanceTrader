372cadfa735ed8ff0475e706c647ee13370861b1
Sergey <20flint12@gmail.com>
2023-05-06 16:30:05 +0200

    sh

--- ptb_binance_monitor.sh
+++ ptb_binance_monitor.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-#cd /home/ubuntu/BinanceTrader
-#read
-
-tmux send-keys -t ptb_binance_monitor "cd /home/ubuntu/BinanceTrader" C-m
-
-echo "--- run env ---"
-tmux send-keys -t ptb_binance_monitor "source venv310/local/bin/activate" C-m
-
-echo "--- run python script ---"
-tmux send-keys -t ptb_binance_monitor "python -m src.PTB_bot.ptb_binance_monitor" C-m
-read
-
 if [[ -n "$TMUX" ]]; then
   echo "You are currently in a tmux session."
+
+  cd /home/ubuntu/BinanceTrader
+  source venv310/local/bin/activate
+  python -m src.PTB_bot.ptb_binance_monitor
+  read
+
 else
   echo "You are not currently in a tmux session."
+
+  tmux send-keys -t ptb_binance_monitor "cd /home/ubuntu/BinanceTrader" C-m
+
+  echo "--- run env ---"
+  tmux send-keys -t ptb_binance_monitor "source venv310/local/bin/activate" C-m
+
+  echo "--- run python script ---"
+  tmux send-keys -t ptb_binance_monitor "python -m src.PTB_bot.ptb_binance_monitor" C-m
+  read
+
   echo "--- attach session ---"
   tmux attach-session -t ptb_binance_monitor
 fi
